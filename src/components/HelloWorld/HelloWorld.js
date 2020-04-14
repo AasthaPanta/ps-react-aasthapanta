c015a8fecfd6cd3b8382ad44a8fac98e67cd6de8
Aastha Panta <apanta@weycorp.com>
2020-04-14 09:50:01 +0545

    documentation generation setup done

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+/**
+ * A component that says Hello with a custom message
+ */
+const HelloWorld = ({message}) => {
+    return(
+        <div>Hello {message}!!</div>
+    );
+}
+
+HelloWorld.propTypes = {
+    /**
+     * Message to display
+     */
+    message: PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+    message: 'World!!'
+}
+
+export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/**
+ * Custom Message
+ */
+const ExampleHelloWorld = () => {
+    return(
+        <HelloWorld message="My Universe" />
+    );
+}
+
+export default ExampleHelloWorld;
