4c5c88fac8b18f68b77066b73cfc14cc1584f7bc
Aastha Panta <apanta@weycorp.com>
2020-04-14 14:49:15 +0545

    organism level reusable components updated

---
+++ src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import RegistrationForm from 'ps-react/RegistrationForm';
+
+class ExampleRegistrationForm extends React.Component{
+
+    onSubmit = (user) =>{
+        console.log(user);
+    }
+    
+    render(){
+        return(
+            <RegistrationForm onSubmit={this.onSubmit} />
+        );
+    }
+
+}
+
+export default ExampleRegistrationForm;
