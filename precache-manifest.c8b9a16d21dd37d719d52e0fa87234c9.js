6810094620a3cfd008ca276771c039677085a3a8
Aastha Panta <apanta@weycorp.com>
2020-04-17 14:43:54 +0545

    Updates

---
+++ precache-manifest.c8b9a16d21dd37d719d52e0fa87234c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76affe64e21d0c64670eb8a4fa354cfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ad60c17480b974ec7f",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "22a16d20373e0db5d6b0",
+    "url": "/static/css/main.f54d1872.chunk.css"
+  },
+  {
+    "revision": "76ad60c17480b974ec7f",
+    "url": "/static/js/2.e73e8493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e73e8493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a16d20373e0db5d6b0",
+    "url": "/static/js/main.78991c9f.chunk.js"
+  },
+  {
+    "revision": "299183898ff5021b1939",
+    "url": "/static/js/runtime-main.8cd38edd.js"
+  }
+]);
